65f29a1b7fa9c0ba649b16d6b00a92db756f070a
Anindita Pal <machome@Aninditas-MacBook-Pro.local>
2019-02-07 00:51:59 -0800

    Removed the out folder.

--- epi_judge_java/epi/BuyAndSellStockTwice.java
+++ epi_judge_java/epi/BuyAndSellStockTwice.java
@@ -1,6 +1,9 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class BuyAndSellStockTwice {
   @EpiTest(testDataFile = "buy_and_sell_stock_twice.tsv")
@@ -8,7 +11,7 @@ public class BuyAndSellStockTwice {
     Double[] buyProfit = new Double[prices.size()];
     Double[] sellProfit = new Double[prices.size()];
 
-    Double maxProfit=0.0, minPrice=Double.MAX_VALUE, maxPrice=Double.MIN_VALUE;
+    double maxProfit=0.0, minPrice=Double.MAX_VALUE, maxPrice=Double.MIN_VALUE;
 
     for(int i=0; i < prices.size(); i++) {
         minPrice = Math.min(minPrice, prices.get(i));
@@ -23,7 +26,6 @@ public class BuyAndSellStockTwice {
         buyProfit[i] = maxProfit;
     }
 
-    maxProfit = 0.0;
     for(int i=1; i < prices.size(); i++) {
         maxProfit = Math.max(maxProfit, buyProfit[i] + sellProfit[i-1]);
     }
@@ -32,6 +34,8 @@ public class BuyAndSellStockTwice {
   }
 
   public static void main(String[] args) {
+
+//      buyAndSellStockTwice(Arrays.asList(0.1, 0.2));
     System.exit(
         GenericTest
             .runFromAnnotations(args, "BuyAndSellStockTwice.java",
