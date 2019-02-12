b7750cdc5c8084584f7b4edcaeba99aad42a93c0
Anindita Pal <machome@Aninditas-MacBook-Pro.local>
2019-02-11 23:44:44 -0800

    6.8

--- epi_judge_java/epi/PrimeSieve.java
+++ epi_judge_java/epi/PrimeSieve.java
@@ -1,13 +1,33 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class PrimeSieve {
   @EpiTest(testDataFile = "prime_sieve.tsv")
   // Given n, return all primes up to and including n.
   public static List<Integer> generatePrimes(int n) {
-    // TODO - you fill in here.
-    return null;
+
+        if (n <= 0) {
+            return new ArrayList<>();
+        }
+
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=2; i <= n; i++) {
+            if (isPrime[i]) {
+                result.add(i);
+
+                for(int j=2; j*i <= n; j++) {
+                    isPrime[j*i] = false;
+                }
+            }
+        }
+        return result;
   }
 
   public static void main(String[] args) {
