29a56fec188c965f5a2b50f76df23dcaa42de202
Anindita Pal <machome@Aninditas-MacBook-Pro.local>
2019-02-15 00:50:40 -0800

    6.10

--- epi_judge_java/epi/NextPermutation.java
+++ epi_judge_java/epi/NextPermutation.java
@@ -1,12 +1,33 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 public class NextPermutation {
   @EpiTest(testDataFile = "next_permutation.tsv")
   public static List<Integer> nextPermutation(List<Integer> perm) {
-    // TODO - you fill in here.
-    return null;
+    int breakPoint=perm.size()-1;
+
+    while (breakPoint > 0 && perm.get(breakPoint) <= perm.get(breakPoint-1)) {
+        breakPoint--;
+    }
+
+    if (breakPoint == 0) {
+        return new ArrayList<>();
+    }
+
+    int pivot=breakPoint-1;
+    breakPoint=perm.size()-1;
+
+    while(perm.get(breakPoint) <= perm.get(pivot)) {
+        breakPoint--;
+    }
+
+    Collections.swap(perm, pivot, breakPoint);
+    Collections.sort(perm.subList(pivot+1, perm.size()));
+    return perm;
   }
 
   public static void main(String[] args) {
